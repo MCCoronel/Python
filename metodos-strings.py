a9e94bb0fd328d4a6109ea24f73354b48d6c9948
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-11-18 18:18:12 -0300

    metodos Strings

---
+++ metodos-strings.py
@@ -0,0 +1,10 @@
+texto = "hola cele"
+resultado = texto.index("cele",0,20)
+print(resultado)
+
+palabra = "ordenador"
+print(palabra[5])
+
+texto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+fragmento = texto[::-2]
+print(fragmento)
